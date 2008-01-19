0a73f4f6780180ca218642fd3a390eddafe82f8f
Marcin Sztolcman <marcin@urzenia.net>
2008-01-19 15:27:12 +0000

    - add files to repo

---
+++ genpass.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+"""
+	version: 0.3.1
+	date: 2004-10-20
+	author: Marcin Sztolcman <marcin@urzenia.net>
+	license: GPL
+	desc: generowanie hasel z podanych zakresow znakow
+"""
+
+import random, sys
+from getopt import gnu_getopt as getopt
+
+USAGE = '''%(prog)s [-l|--letters] [-b|--bigletters] [-d|--digits] [-c|--chars] [-s|--special] [-g|--length length] [-q|--quant quant] [-h|--help]
+DEFAULT:
+    %(prog)s --letters --length=8 --quant=1''' % {'prog': sys.argv[0]}
+
+def exit(code, msg):
+  print >>sys.stderr, msg
+  raise SystemExit, code
+
+
+
+# opcje
+sh_opts='lbdcsq:g:h'
+lg_opts=('letters', 'bigletters', 'digits', 'chars', 'special', 'quant=', 'length=', 'help')
+try:    opts, args = getopt(sys.argv[1:], sh_opts, lg_opts)
+except: exit(1, USAGE)
+
+quant = 1
+length = 8
+
+# zestawy znakow
+z1=r'qwertyuiopasdfghjklzxcvbnm'
+z2=r'QWERTYUIOPASDFGHJKLZXCVBNM'
+z3=r'1234567890'
+z4=r'!@#$%^&*_+=-~;:.>,<?'
+z5=r'''`(){}[]'"\|/'''
+
+# ustalamy zestaw z ktorego losujemy
+chars = r''
+for o, a in opts:
+  if o in ('-h', '--help'):
+    exit(0, USAGE)
+  elif o in ('-l', '--letters'):
+    chars += z1
+  elif o in ('-b', '--bigletters'):
+    chars += z2
+  elif o in ('-d', '--digits'):
+    chars += z3
+  elif o in ('-c', '--chars'):
+    chars += z4
+  elif o in ('-s', '--special'):
+    chars += z5
+  elif o in ('-q', '--quant'):
+    quant = a
+  elif o in ('-g', '--length'):
+    length = a
+
+try:
+  length = int(length)
+  quant = int(quant)
+except:
+  exit(2, USAGE)
+
+all_chars = len(chars)
+if all_chars == 0:
+  chars = z1[:]
+  all_chars = len(chars)
+
+
+
+random.seed()
+for j in xrange(quant):
+	passwd = ''
+	for i in xrange(length): passwd += random.choice(chars)
+	print passwd
+
+'''
+ChangeLog:
+2005-04-16 - random.seed() wyrzucone poza petle + kosmetyka
+2005-03-23 - drobne poprawki
+'''
---
+++ mke.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os.path
+import re
+import subprocess
+
+import gconf
+
+class MetacityKeybindingsEditor (object):
+	TITLE	= 'Metacity Keybindings Editor (c) 2008 Marcin Sztolcman'
+	def __init__ (self):
+		self.client = gconf.client_get_default ()
+		self.current_keybindings = {}
+
+		self.get_current_keybindings ()
+
+	def get_current_keybindings (self):
+		for d in self.client.all_entries ('/apps/metacity/global_keybindings'):
+			k = os.path.basename (d.get_key ())
+			m = re.match ('^run_command_(\d+)$', k)
+			if not m:
+				continue
+			m = int (m.groups ()[0])
+
+			v = d.get_value ().to_string ()
+			self.current_keybindings[m] = {'keybinding': v}
+
+		for d in self.client.all_entries ('/apps/metacity/keybinding_commands'):
+			k = os.path.basename (d.get_key ())
+			m = re.match ('^command_(\d+)$', k)
+			if not m:
+				continue
+			m = int (m.groups ()[0])
+
+			v = d.get_value ().to_string ()
+			self.current_keybindings[m]['action'] = v
+
+	def zenity_list (self):
+		cmd	= ['/usr/bin/zenity', '--title='+self.TITLE, '--width=700', '--height=420', '--list', '--print-column=ALL', '--text=Wybierz komendę do edycji i kliknij "OK" (aby zakończyć kliknij anuluj):', '--column=Komenda', '--column=Akcja', '--column=Skrót']
+		for d in self.current_keybindings.keys ():
+			cmd += [str (d), self.current_keybindings[d]['action'], self.current_keybindings[d]['keybinding']]
+
+		ret = self.system (cmd)[0].strip ()
+		ret = ret.split ('|')
+
+		return ret
+
+	def zenity_action (self, data):
+		cmd		= ['/usr/bin/zenity', '--title='+self.TITLE, '--entry', '--entry-text='+data, '--text=Podaj pełną ścieżkę do aplikacji wraz z parametrami:']
+
+		ret = self.system (cmd)[0].strip ()
+		return ret
+
+	def zenity_keybinding (self, data):
+		cmd		= ['/usr/bin/zenity', '--title='+self.TITLE, '--entry', '--entry-text='+data, '--text=Podaj skrót jaki chcesz przypisać do wybranej akcji (klawisze specjale: <Control>, <Shift>, <Alt>, F1-F12']
+
+		ret = self.system (cmd)[0].strip ()
+		return ret
+
+	def gconf_set (self, cmdno, action, keybinding):
+		if not keybinding:
+			keybinding = 'disabled'
+
+		self.client.set_string ('/apps/metacity/keybinding_commands/command_'+str(cmdno), action)
+		self.client.set_string ('/apps/metacity/global_keybindings/run_command_'+str(cmdno), keybinding)
+
+		self.current_keybindings[int (cmdno)] = {'action':action, 'keybinding':keybinding}
+#        self.get_current_keybindings ()
+
+	def system (self, cmd):
+		return subprocess.Popen (cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate ()
+
+
+if __name__ == '__main__':
+	S = MetacityKeybindingsEditor ()
+	while True:
+		elem = S.zenity_list ()
+		if len (elem) % 3:
+			break
+
+		action		= S.zenity_action (elem[1])
+		keybinding	= S.zenity_keybinding (elem[2])
+		S.gconf_set (elem[0], action, keybinding)
+
+# vim: ft=python
