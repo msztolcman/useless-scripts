9c45a35cfd37f00b05595919dddc23c816265b11
Marcin Sztolcman <marcin@urzenia.net>
2012-05-26 00:04:24 +0200

    utility to encode/decode URI data

---
+++ uri.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals
+import os, os.path
+import sys
+
+import argparse
+try:
+    from urllib.parse import quote_plus, unquote_plus, quote, unquote
+except ImportError:
+    from urllib import quote_plus, unquote_plus, quote, unquote
+
+__version__   = 'version 0.1'
+__author__    = 'Marcin Sztolcman <marcin@urzenia.net>'
+__copyright__ = '(r) 2012'
+__program__   = 'uri.py - encode/decode URI data'
+__date__      = '2012-05-26'
+__license__   = 'GPL v.2'
+
+__desc__      = '''%(desc)s
+%(author)s %(copyright)s
+license: %(license)s
+version %(version)s (%(date)s)''' % {
+  'desc': __program__,
+  'author': __author__,
+  'copyright': __copyright__,
+  'license': __license__,
+  'version': __version__,
+  'date': __date__
+}
+
+def main ():
+    parser = argparse.ArgumentParser (description='URI data encode or decode')
+    parser.add_argument ('-d', '--decode', action='store_true', help='decode data if given')
+    parser.add_argument ('-s', '--space_as_plus', action='store_false', help='encode/decode space as plus sign instead of %%20')
+    parser.add_argument ('input', help='data to process')
+
+    args = parser.parse_args ()
+
+    if args.decode:
+        if args.space_as_plus:
+            func = unquote
+        else:
+            func = unquote_plus
+    else:
+        if args.space_as_plus:
+            func = quote
+        else:
+            func = quote_plus
+
+    if not args.input:
+        data = sys.stdin.read ().rstrip (os.linesep)
+    else:
+        data = args.input
+
+    print (func (data))
+
+if __name__ == '__main__':
+    main ()
