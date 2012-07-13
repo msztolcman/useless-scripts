c836982f448a20d1bb055dc14ee0805dd46b59f2
Marcin Sztolcman <marcin@urzenia.net>
2012-07-13 19:36:54 +0200

    added info about file

--- uri.py
+++ uri.py
@@ -2,6 +2,26 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import print_function, unicode_literals
+
+__version__   = 'version 0.1'
+__author__    = 'Marcin Sztolcman <marcin@urzenia.net>'
+__copyright__ = '(r) 2012'
+__program__   = 'uri.py - uri encode/uri decode data'
+__date__      = '2012-07-13'
+__license__   = 'GPL v.2'
+
+__desc__      = '''%(desc)s
+%(author)s %(copyright)s
+license: %(license)s
+version %(version)s (%(date)s)''' % {
+  'desc': __program__,
+  'author': __author__,
+  'copyright': __copyright__,
+  'license': __license__,
+  'version': __version__,
+  'date': __date__
+}
+
 import os, os.path
 import sys
 
